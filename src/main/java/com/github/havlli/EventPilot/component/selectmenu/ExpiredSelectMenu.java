ad3ea70da2d60b257cc74adbd54b899607e6ffd4
Havlli <havlliczech@gmail.com>
2023-07-28 19:17:04 +0200

    added new validation - added validation to DiscordService to check already expired events on client side - added custom component ExpiredSelectMenu for tagging expired events - new properties in configuration file

---
+++ src/main/java/com/github/havlli/EventPilot/component/selectmenu/ExpiredSelectMenu.java
@@ -0,0 +1,31 @@
+package com.github.havlli.EventPilot.component.selectmenu;
+
+import com.github.havlli.EventPilot.component.SelectMenuComponent;
+import discord4j.core.object.component.ActionRow;
+import discord4j.core.object.component.SelectMenu;
+
+public class ExpiredSelectMenu implements SelectMenuComponent {
+
+    private static final String CUSTOM_ID = "expired";
+    private static final String PLACEHOLDER = "This event already expired!";
+
+    @Override
+    public String getCustomId() {
+        return CUSTOM_ID;
+    }
+
+    @Override
+    public ActionRow getActionRow() {
+        return getDisabledRow();
+    }
+
+    @Override
+    public ActionRow getDisabledRow() {
+        return ActionRow.of(buildSelectMenu().disabled(true));
+    }
+
+    private SelectMenu buildSelectMenu() {
+        return SelectMenu.of(CUSTOM_ID, SelectMenu.Option.of("ignored", "ignored"))
+                .withPlaceholder(PLACEHOLDER);
+    }
+}
--- src/main/java/com/github/havlli/EventPilot/core/DiscordService.java
+++ src/main/java/com/github/havlli/EventPilot/core/DiscordService.java
@@ -1,9 +1,10 @@
 package com.github.havlli.EventPilot.core;
 
-import com.github.havlli.EventPilot.component.selectmenu.RaidSelectMenu;
+import com.github.havlli.EventPilot.component.selectmenu.ExpiredSelectMenu;
 import com.github.havlli.EventPilot.entity.event.Event;
 import discord4j.common.util.Snowflake;
 import discord4j.core.GatewayDiscordClient;
+import discord4j.core.object.entity.Message;
 import discord4j.core.spec.MessageEditSpec;
 import discord4j.rest.http.client.ClientException;
 import org.slf4j.Logger;
@@ -12,6 +13,7 @@ import org.springframework.stereotype.Service;
 import reactor.core.publisher.Mono;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class DiscordService {
@@ -26,7 +28,7 @@ public class DiscordService {
     public void deactivateEvents(List<Event> events) {
 
         MessageEditSpec editedMessage = MessageEditSpec.builder()
-                .addComponent(new RaidSelectMenu().getDisabledRow())
+                .addComponent(new ExpiredSelectMenu().getActionRow())
                 .build();
 
         events.forEach(event -> {
@@ -34,7 +36,12 @@ public class DiscordService {
             Snowflake channelId = Snowflake.of(event.getDestinationChannelId());
 
             client.getMessageById(channelId, messageId)
-                    .flatMap(message -> message.edit(editedMessage))
+                    .flatMap(message -> {
+
+                        if (!isAlreadyDeactivated(message)) return message.edit(editedMessage);
+
+                        return Mono.empty();
+                    })
                     .onErrorResume(ClientException.class, e -> {
 
                         String errorMessage = "Message {%s} was not found in channel {%s}"
@@ -45,5 +52,20 @@ public class DiscordService {
                     })
                     .subscribe();
         });
+    }
+
+    private boolean isAlreadyDeactivated(Message message) {
+        Optional<String> customId = message.getComponents().stream()
+                .findFirst()
+                .flatMap(layoutComponent -> layoutComponent.getChildren().stream()
+                        .findFirst())
+                .flatMap(messageComponent -> messageComponent
+                        .getData()
+                        .customId()
+                        .toOptional());
+
+        if (customId.isEmpty()) return false;
+
+        return customId.get().equals("expired");
     }
 }
