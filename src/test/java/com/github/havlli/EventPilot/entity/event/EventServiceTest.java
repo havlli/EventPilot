b164e90c0c13b53ab33fc15cdd0815232f27e636
Havlli <havlliczech@gmail.com>
2023-08-01 18:23:31 +0200

    new test cases for EventService.java

---
+++ src/test/java/com/github/havlli/EventPilot/entity/event/EventServiceTest.java
@@ -0,0 +1,87 @@
+package com.github.havlli.EventPilot.entity.event;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import java.util.List;
+
+import static org.mockito.Mockito.*;
+
+class EventServiceTest {
+
+    private AutoCloseable autoCloseable;
+    @Mock
+    private EventDAO eventDAO;
+    @InjectMocks
+    private EventService underTest;
+
+
+    @BeforeEach
+    void setUp() {
+        autoCloseable = MockitoAnnotations.openMocks(this);
+    }
+
+    @AfterEach
+    void tearDown() throws Exception {
+        autoCloseable.close();
+    }
+
+    @Test
+    void saveEvent() {
+        // Arrange
+        Event event = mock(Event.class);
+
+        // Act
+        underTest.saveEvent(event);
+
+        // Assert
+        verify(eventDAO, times(1)).insertEvent(event);
+    }
+
+    @Test
+    void getAllEvents() {
+        // Act
+        underTest.getAllEvents();
+
+        // Assert
+        verify(eventDAO, times(1)).fetchEvents();
+    }
+
+    @Test
+    void deleteEvent() {
+        // Arrange
+        Event event = mock(Event.class);
+
+        // Act
+        underTest.deleteEvent(event);
+
+        // Assert
+        verify(eventDAO, times(1)).deleteEvent(event);
+    }
+
+    @Test
+    void deleteAllEvents() {
+        // Arrange
+        Event event = mock(Event.class);
+        List<Event> eventList = List.of(event);
+
+        // Act
+        underTest.deleteAllEvents(eventList);
+
+        // Assert
+        verify(eventDAO, times(1)).deleteAllEvents(eventList);
+    }
+
+    @Test
+    void getExpiredEvents() {
+        // Act
+        underTest.getExpiredEvents();
+
+        // Assert
+        verify(eventDAO, times(1)).fetchExpiredEvents();
+    }
+}
