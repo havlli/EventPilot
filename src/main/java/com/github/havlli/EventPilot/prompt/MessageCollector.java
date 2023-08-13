abc83190e0c3142f7f402888f400c4cfb840076b
Havlli <havlliczech@gmail.com>
2023-08-13 23:08:56 +0200

    changed from chained Mono to Flux

--- src/main/java/com/github/havlli/EventPilot/prompt/MessageCollector.java
+++ src/main/java/com/github/havlli/EventPilot/prompt/MessageCollector.java
@@ -3,16 +3,21 @@ package com.github.havlli.EventPilot.prompt;
 import discord4j.core.object.entity.Message;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Component
+@Scope("prototype")
 public class MessageCollector {
 
-    private static final Logger logger = LoggerFactory.getLogger(MessageCollector.class);
+    private static final Logger LOG = LoggerFactory.getLogger(MessageCollector.class);
     private final List<Message> messageList;
 
     public MessageCollector() {
@@ -23,20 +28,24 @@ public class MessageCollector {
         messageList.add(message);
     }
 
-    public Mono<Void> cleanup() {
-        Mono<Void> chainedMono = Mono.empty();
+    public Flux<Void> cleanup() {
 
-        for(Message message : messageList) {
-            chainedMono = chainedMono.and(deleteMessage(message));
-        }
+        if (messageList.isEmpty()) return Flux.empty();
+
+        List<Mono<Void>> monoList = messageList.stream()
+                .map(this::deleteMessage)
+                .collect(Collectors.collectingAndThen(Collectors.toList(), list -> {
+                    Collections.reverse(list);
+                    return list;
+                }));
         messageList.clear();
 
-        return chainedMono;
+        return Flux.concat(monoList);
     }
 
     private Mono<Void> deleteMessage(Message message) {
         return message.delete().onErrorResume(error -> {
-            logger.error("Error occurred while deleting message {%s}".formatted(message.getId().asString()), error);
+            LOG.error("Error occurred while deleting message {}\n{}", message.getId().asString(), error.getStackTrace());
             return Mono.empty();
         });
     }
