a8c28ba036f2c789e686bc7c05efb09d4c21c75e
Havlli <havlliczech@gmail.com>
2023-08-13 20:13:48 +0200

    - introduced null checks on required fields in order to build valid object - refactored response handling logic into function

--- src/main/java/com/github/havlli/EventPilot/prompt/TextPromptMono.java
+++ src/main/java/com/github/havlli/EventPilot/prompt/TextPromptMono.java
@@ -9,14 +9,20 @@ import discord4j.core.event.domain.message.MessageCreateEvent;
 import discord4j.core.object.entity.channel.MessageChannel;
 import discord4j.core.spec.InteractionReplyEditSpec;
 import discord4j.core.spec.MessageCreateSpec;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import reactor.core.publisher.Mono;
 
+import javax.validation.constraints.NotNull;
 import java.util.List;
+import java.util.Objects;
 import java.util.function.Consumer;
+import java.util.function.Function;
 import java.util.function.Predicate;
 
 public class TextPromptMono<T extends Event> {
 
+    private final static Logger LOG = LoggerFactory.getLogger(TextPromptMono.class);
     private final Mono<? extends MessageChannel> messageChannel;
     private final MessageCollector messageCollector;
     private final MessageCreateSpec messageCreateSpec;
@@ -52,6 +58,7 @@ public class TextPromptMono<T extends Event> {
                         responseMono = responseMono.onErrorResume(errorClass, e -> messageChannel
                                 .flatMap(channel -> channel.createMessage(onErrorMessage))
                                 .flatMap(message -> {
+                                    LOG.error("threw error {}\n{}", errorClass.getCanonicalName(), e.getStackTrace());
                                     messageCollector.collect(message);
                                     return this.mono();
                                 }));
@@ -60,55 +67,49 @@ public class TextPromptMono<T extends Event> {
                     return responseMono;
                 });
     }
-
     private Mono<T> constructResponse() {
-        if (eventClass.getCanonicalName().equals(SelectMenuInteractionEvent.class.getCanonicalName())) {
-            ExpiredSelectMenu expiredMenu = new ExpiredSelectMenu();
-            return client.getEventDispatcher().on(eventClass)
-                    .filter(eventPredicate)
-                    .next()
-                    .flatMap(event -> {
-                        eventProcessor.accept(event);
-                        SelectMenuInteractionEvent interactionEvent = ((SelectMenuInteractionEvent) event);
-                        return interactionEvent.deferEdit()
-                                .then(interactionEvent.editReply(InteractionReplyEditSpec.builder()
-                                        .components(List.of(expiredMenu.getDisabledRow()))
-                                        .build()))
-                                .then(Mono.just(event));
-                    });
+        return client.getEventDispatcher().on(eventClass)
+                .filter(eventPredicate)
+                .next()
+                .flatMap(event -> {
+                    eventProcessor.accept(event);
+                    return interactionResponseMono().apply(event);
+                });
+    }
 
-        } else if (eventClass.getCanonicalName().equals(ButtonInteractionEvent.class.getCanonicalName())) {
-            return client.getEventDispatcher().on(eventClass)
-                    .filter(eventPredicate)
-                    .next()
-                    .flatMap(event -> {
-                        eventProcessor.accept(event);
-                        ButtonInteractionEvent interactionEvent = ((ButtonInteractionEvent) event);
-                        return interactionEvent.deferEdit()
-                                .then(interactionEvent.getInteractionResponse().deleteInitialResponse())
-                                .then(Mono.just(event));
-                    });
-
-        } else if (eventClass.getCanonicalName().equals(MessageCreateEvent.class.getCanonicalName())) {
-            return client.getEventDispatcher().on(eventClass)
-                    .filter(eventPredicate)
-                    .next()
-                    .flatMap(event -> {
-                        eventProcessor.accept(event);
-                        return Mono.just(event);
-                    });
-        } else {
-            throw new IllegalStateException(eventClass + " not supported!");
-        }
+    private Function<T, Mono<T>> interactionResponseMono() {
+        return event -> {
+            if (event instanceof SelectMenuInteractionEvent selectMenuEvent) {
+                ExpiredSelectMenu expiredMenu = new ExpiredSelectMenu();
+                return selectMenuEvent.deferEdit()
+                        .then(selectMenuEvent.editReply(InteractionReplyEditSpec.builder()
+                                .components(List.of(expiredMenu.getDisabledRow()))
+                                .build()))
+                        .then(Mono.just(event));
+            } else if (event instanceof ButtonInteractionEvent buttonEvent) {
+                return buttonEvent.deferEdit()
+                        .then(buttonEvent.getInteractionResponse().deleteInitialResponse())
+                        .then(Mono.just(event));
+            } else if (event instanceof MessageCreateEvent) {
+                return Mono.just(event);
+            }
+            throw new IllegalStateException("%s not supported operation".formatted(eventClass));
+        };
     }
 
     public static class Builder<T extends Event> {
+        @NotNull
         private Mono<? extends MessageChannel> messageChannel;
         private MessageCollector messageCollector;
+        @NotNull
         private MessageCreateSpec messageCreateSpec;
+        @NotNull
         private final GatewayDiscordClient client;
+        @NotNull
         private final Class<T> eventClass;
+        @NotNull
         private Consumer<T> eventProcessor;
+        @NotNull
         private Predicate<T> eventPredicate;
         private String onErrorMessage;
         private Class<? extends Throwable> errorClass;
@@ -150,6 +151,12 @@ public class TextPromptMono<T extends Event> {
         }
 
         public TextPromptMono<T> build() {
+            Objects.requireNonNull(messageChannel, "messageChannel is required");
+            Objects.requireNonNull(messageCreateSpec, "messageCreateSpec is required");
+            Objects.requireNonNull(eventClass, "eventClass is required");
+            Objects.requireNonNull(client, "client is required");
+            Objects.requireNonNull(eventProcessor, "eventProcessor is required");
+            Objects.requireNonNull(eventPredicate, "eventPredicate is required");
             return new TextPromptMono<>(this);
         }
     }
