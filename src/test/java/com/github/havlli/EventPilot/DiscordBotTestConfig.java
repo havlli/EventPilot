3d841d482040377ef1406275333dd8497da94191
Havlli <havlliczech@gmail.com>
2023-08-19 18:51:09 +0200

    tests for Client.java and introduced new abstract test config class for configuring BotTest environment

---
+++ src/test/java/com/github/havlli/EventPilot/DiscordBotTestConfig.java
@@ -0,0 +1,5 @@
+package com.github.havlli.EventPilot;
+
+public abstract class DiscordBotTestConfig {
+    protected static final String token = "test-environment-token-secret";
+}
---
+++ src/test/java/com/github/havlli/EventPilot/core/ClientTest.java
@@ -0,0 +1,41 @@
+package com.github.havlli.EventPilot.core;
+
+import com.github.havlli.EventPilot.DiscordBotTestConfig;
+import discord4j.common.util.Snowflake;
+import discord4j.core.GatewayDiscordClient;
+import discord4j.rest.RestClient;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class ClientTest extends DiscordBotTestConfig {
+
+    private Client underTest;
+
+    @BeforeEach
+    void setUp() {
+        underTest = new Client(token);
+    }
+
+    @Test
+    public void discordClient_ConnectsToTestBot() {
+        // Act
+        GatewayDiscordClient gatewayDiscordClient = underTest.discordClient();
+
+        // Assert
+        assertThat(gatewayDiscordClient.getSelfId()).isEqualTo(Snowflake.of(1142488932903309383L));
+    }
+
+    @Test
+    public void restClient_ReturnsRestClient() {
+        // Arrange
+        GatewayDiscordClient gatewayDiscordClient = underTest.discordClient();
+
+        // Act
+        RestClient restClient = underTest.restClient(gatewayDiscordClient);
+
+        // Assert
+        assertThat(restClient).isNotNull();
+    }
+}
