77506af24d6f91094905173dfac08dffe98720d8
Havlli <havlliczech@gmail.com>
2023-08-23 23:43:23 +0200

    changed to simple unit testing of the class instead of loading entire SpringBoot application context

--- src/test/java/com/github/havlli/EventPilot/core/ConfigTest.java
+++ src/test/java/com/github/havlli/EventPilot/core/ConfigTest.java
@@ -1,21 +1,25 @@
 package com.github.havlli.EventPilot.core;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
 
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
-@SpringBootTest
 class ConfigTest {
 
-    @Autowired
-    private PathMatchingResourcePatternResolver patternResolver;
+    private Config underTest;
 
+    @BeforeEach
+    void setUp() {
+        underTest = new Config();
+    }
     @Test
     void pathMatchingResourcePatternResolver() {
+        // Act
+        PathMatchingResourcePatternResolver actual = underTest.pathMatchingResourcePatternResolver();
+
         // Assert
-        assertNotNull(patternResolver);
+        assertNotNull(actual);
     }
 }
