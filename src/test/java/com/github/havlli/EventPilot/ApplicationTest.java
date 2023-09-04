82736e162175ad78914d3965e440e98e86965c12
Havlli <havlliczech@gmail.com>
2023-09-04 23:00:47 +0200

    fixing entities and maximizing test coverage

--- src/test/java/com/github/havlli/EventPilot/ApplicationTest.java
+++ src/test/java/com/github/havlli/EventPilot/ApplicationTest.java
@@ -7,7 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.ApplicationContext;
 
-@SpringBootTest(classes = Application.class)
+@SpringBootTest
 class ApplicationTest {
 
     private static final Logger LOG = LoggerFactory.getLogger(ApplicationTest.class);
