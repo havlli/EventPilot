8d00a1a0397c9e285a20044d59c58386d82196f1
Havlli <havlliczech@gmail.com>
2023-09-16 19:03:22 +0200

    refactoring

--- src/main/java/com/github/havlli/EventPilot/prompt/PromptFilter.java
+++ src/main/java/com/github/havlli/EventPilot/prompt/PromptFilter.java
@@ -3,6 +3,7 @@ package com.github.havlli.EventPilot.prompt;
 import com.github.havlli.EventPilot.component.ButtonRowComponent;
 import com.github.havlli.EventPilot.component.SelectMenuComponent;
 import discord4j.core.event.domain.interaction.ButtonInteractionEvent;
+import discord4j.core.event.domain.interaction.ComponentInteractionEvent;
 import discord4j.core.event.domain.interaction.SelectMenuInteractionEvent;
 import discord4j.core.event.domain.message.MessageCreateEvent;
 import discord4j.core.object.entity.User;
@@ -16,23 +17,35 @@ public class PromptFilter {
 
     public Predicate<SelectMenuInteractionEvent> selectInteractionEvent(SelectMenuComponent selectMenuComponent, User user) {
         return event -> {
-            boolean isSameUser = event.getInteraction().getUser().equals(user);
-            boolean isComponent = event.getCustomId().equals(selectMenuComponent.getCustomId());
-            return isSameUser && isComponent;
+            Predicate<ComponentInteractionEvent> userPredicate = isUserInvolvedInInteraction(user);
+            Predicate<ComponentInteractionEvent> componentPredicate = isComponentInvolvedInInteraction(selectMenuComponent);
+            return userPredicate.and(componentPredicate).test(event);
         };
     }
 
     public Predicate<ButtonInteractionEvent> buttonInteractionEvent(ButtonRowComponent buttonRowComponent, User user) {
         return event -> {
-            boolean isSameUser = event.getInteraction().getUser().equals(user);
-            boolean hasComponents = buttonRowComponent.getCustomIds()
-                    .stream()
-                    .anyMatch(customId -> customId.equals(event.getCustomId()));
-            return isSameUser && hasComponents;
+            Predicate<ComponentInteractionEvent> userPredicate = isUserInvolvedInInteraction(user);
+            Predicate<ComponentInteractionEvent> componentPredicate = isComponentInvolvedInInteraction(buttonRowComponent);
+            return userPredicate.and(componentPredicate).test(event);
         };
     }
 
     public Predicate<MessageCreateEvent> isMessageAuthor(User user) {
         return event -> event.getMessage().getAuthor().equals(Optional.of(user));
+    }
+
+    private Predicate<ComponentInteractionEvent> isUserInvolvedInInteraction(User user) {
+        return event -> event.getInteraction().getUser().equals(user);
+    }
+
+    private Predicate<ComponentInteractionEvent> isComponentInvolvedInInteraction(SelectMenuComponent selectMenuComponent) {
+        return event -> event.getCustomId().equals(selectMenuComponent.getCustomId());
+    }
+
+    private Predicate<ComponentInteractionEvent> isComponentInvolvedInInteraction(ButtonRowComponent buttonRowComponent) {
+        return event -> buttonRowComponent.getCustomIds()
+                .stream()
+                .anyMatch(customId -> customId.equals(event.getCustomId()));
     }
 }
