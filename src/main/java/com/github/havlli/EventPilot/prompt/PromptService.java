a028793003e8d0cfc9a380b7e987c0178b178476
Havlli <havlliczech@gmail.com>
2023-09-17 19:45:52 +0200

    refactoring

--- src/main/java/com/github/havlli/EventPilot/prompt/PromptService.java
+++ src/main/java/com/github/havlli/EventPilot/prompt/PromptService.java
@@ -3,10 +3,12 @@ package com.github.havlli.EventPilot.prompt;
 import discord4j.common.util.Snowflake;
 import discord4j.core.GatewayDiscordClient;
 import discord4j.core.event.domain.interaction.InteractionCreateEvent;
+import discord4j.core.object.command.Interaction;
 import discord4j.core.object.entity.Guild;
 import discord4j.core.object.entity.channel.TextChannel;
 import org.springframework.stereotype.Service;
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 @Service
 public class PromptService {
@@ -18,14 +20,22 @@ public class PromptService {
     }
 
     public Flux<TextChannel> fetchGuildTextChannels(InteractionCreateEvent event) {
-        return event.getInteraction()
-                .getGuild()
+        return getGuild(event)
                 .map(Guild::getId)
                 .flatMapMany(guildId -> client.getGuildChannels(guildId).ofType(TextChannel.class));
     }
 
     public Snowflake fetchGuildId(InteractionCreateEvent event) {
-        return event.getInteraction().getGuildId()
+        return getInteraction(event)
+                .getGuildId()
                 .orElse(Snowflake.of(0));
+    }
+
+    private Interaction getInteraction(InteractionCreateEvent event) {
+        return event.getInteraction();
+    }
+
+    private Mono<Guild> getGuild(InteractionCreateEvent event) {
+        return getInteraction(event).getGuild();
     }
 }
