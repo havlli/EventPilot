a71e5aa0960b069bbb5fb9b928e7499f05bc29ec
Havlli <havlliczech@gmail.com>
2023-09-30 02:35:12 +0200

    fixing issues with property configuration

--- src/main/java/com/github/havlli/EventPilot/session/RedisConfig.java
+++ src/main/java/com/github/havlli/EventPilot/session/RedisConfig.java
@@ -1,15 +1,28 @@
 package com.github.havlli.EventPilot.session;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
-import org.springframework.session.config.annotation.web.http.EnableSpringHttpSession;
 
 @Configuration
-@EnableSpringHttpSession
 public class RedisConfig {
+
+    @Value("${cache.redis.host}")
+    private String redisHost;
+    @Value("${cache.redis.port}")
+    private Integer redisPort;
     @Bean
-    public LettuceConnectionFactory connectionFactory() {
-        return new LettuceConnectionFactory();
+    public LettuceConnectionFactory connectionFactory(RedisStandaloneConfiguration redisStandaloneConfiguration) {
+        redisStandaloneConfiguration.setHostName(redisHost);
+        redisStandaloneConfiguration.setPort(redisPort);
+        redisStandaloneConfiguration.setDatabase(0);
+        return new LettuceConnectionFactory(redisStandaloneConfiguration);
+    }
+
+    @Bean
+    public RedisStandaloneConfiguration redisStandaloneConfiguration() {
+        return new RedisStandaloneConfiguration();
     }
 }
