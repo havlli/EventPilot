d938df6cca3314e8234cdd88d8a8a526ba5c7292
Havlli <havlliczech@gmail.com>
2023-11-21 13:18:42 +0100

    new endpoint for fetching users

--- src/main/java/com/github/havlli/EventPilot/api/UserController.java
+++ src/main/java/com/github/havlli/EventPilot/api/UserController.java
@@ -1,9 +1,11 @@
 package com.github.havlli.EventPilot.api;
 
+import com.github.havlli.EventPilot.entity.user.User;
 import com.github.havlli.EventPilot.entity.user.UserDTO;
 import com.github.havlli.EventPilot.entity.user.UserService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -28,5 +30,12 @@ public class UserController {
 
         return ResponseEntity.ok()
                 .body(userDTOList);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<UserDTO> getUserById(@PathVariable Long id) {
+        User user = userService.findUserById(id);
+        UserDTO userDTO = UserDTO.of(user);
+        return ResponseEntity.ok(userDTO);
     }
 }
--- src/test/java/com/github/havlli/EventPilot/journey/UserControllerIT.java
+++ src/test/java/com/github/havlli/EventPilot/journey/UserControllerIT.java
@@ -4,6 +4,9 @@ import com.github.havlli.EventPilot.TestDatabaseContainer;
 import com.github.havlli.EventPilot.api.ApiErrorResponse;
 import com.github.havlli.EventPilot.api.auth.UserSignupRequest;
 import com.github.havlli.EventPilot.entity.user.User;
+import com.github.havlli.EventPilot.entity.user.UserDTO;
+import com.github.havlli.EventPilot.entity.user.UserRepository;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
@@ -27,8 +30,75 @@ public class UserControllerIT extends TestDatabaseContainer {
 
     @Autowired
     private WebTestClient webTestClient;
+    @Autowired
+    private UserRepository userRepository;
 
     private static final String USER_CONTROLLER_BASE_URI = "/api/users";
+
+    @BeforeEach
+    void setUp() {
+        userRepository.deleteAll();
+    }
+
+    @Test
+    void getUserById_ReturnsUserDTO_WhenUserExists() {
+        // Arrange
+        String bearerToken = signupUser("username", "password", "email");
+
+        UserDTO expected = new UserDTO(null, "username", "email", null);
+        List<User> actualUsers = webTestClient.get()
+                .uri(USER_CONTROLLER_BASE_URI)
+                .accept(MediaType.APPLICATION_JSON)
+                .header(HttpHeaders.AUTHORIZATION, bearerToken)
+                .exchange()
+                .expectStatus().isOk()
+                .expectBodyList(User.class)
+                .returnResult()
+                .getResponseBody();
+        Long userId = actualUsers.stream()
+                .filter(user -> user.getUsername().equals("username"))
+                .findFirst()
+                .map(User::getId)
+                .orElseThrow();
+
+        // Act
+        UserDTO actual = webTestClient.get()
+                .uri(USER_CONTROLLER_BASE_URI + "/" + userId)
+                .accept(MediaType.APPLICATION_JSON)
+                .header(HttpHeaders.AUTHORIZATION, bearerToken)
+                .exchange()
+                .expectStatus().isOk()
+                .expectBody(UserDTO.class)
+                .returnResult()
+                .getResponseBody();
+
+        // Assert
+        assertThat(actual).usingRecursiveComparison()
+                .comparingOnlyFields("username", "email")
+                .isEqualTo(expected);
+    }
+
+    @Test
+    void getUserById_Returns404Error_WhenUserDoesNotExist() {
+        // Arrange
+        String bearerToken = signupUser("username", "password", "email");
+
+        long userId = 3L;
+
+        // Act
+        ApiErrorResponse actual = webTestClient.get()
+                .uri(USER_CONTROLLER_BASE_URI + "/" + userId)
+                .accept(MediaType.APPLICATION_JSON)
+                .header(HttpHeaders.AUTHORIZATION, bearerToken)
+                .exchange()
+                .expectStatus().isNotFound()
+                .expectBody(ApiErrorResponse.class)
+                .returnResult()
+                .getResponseBody();
+
+        // Assert
+        assertThat(actual.httpStatus()).isEqualTo(HttpStatus.NOT_FOUND);
+    }
 
     @Test
     void getAllUsers_ReturnsListOfUserDTOs_WhenAuthenticatedUserRole() {
@@ -73,7 +143,7 @@ public class UserControllerIT extends TestDatabaseContainer {
 
     // Helper methods
     private String signupUser(String username, String password, String email) {
-        UserSignupRequest signupRequest = new UserSignupRequest(username, password, email);
+        UserSignupRequest signupRequest = new UserSignupRequest(username, email, password);
         String jwtToken = webTestClient.post()
                 .uri("/api/auth/signup")
                 .contentType(MediaType.APPLICATION_JSON)
