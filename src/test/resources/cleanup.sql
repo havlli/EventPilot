815d594c94224a79e35c9d32b3c702d9dbd16510
Havlli <havlliczech@gmail.com>
2026-04-25 23:51:38 +0200

    Reset integration test database state

--- src/test/resources/cleanup.sql
+++ src/test/resources/cleanup.sql
@@ -1,3 +1,8 @@
-DELETE FROM participant;
-DELETE FROM event;
-DELETE FROM guild;
+TRUNCATE TABLE participant, event, guild, embed_type, user_role, users, role RESTART IDENTITY CASCADE;
+
+INSERT INTO embed_type (name, structure)
+VALUES ('default', '{"-1":"Absence","-2":"Late","1":"Tank","-3":"Tentative","2":"Melee","3":"Ranged","4":"Healer","5":"Support"}');
+
+INSERT INTO role(name) VALUES('USER');
+INSERT INTO role(name) VALUES('MODERATOR');
+INSERT INTO role(name) VALUES('ADMIN');
